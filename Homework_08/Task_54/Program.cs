870089b9dae74e509bae59f6caffeba4ea952d4d
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:49:31 +0500

    Task_54 is done

---
+++ Homework_08/Task_54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] array = CreateRandom2DArray(3,4);
+
+Print2DArray(array);
+
+Console.WriteLine();
+
+int[,] changeArray = ChangeOrderElementsRows(array);
+
+Print2DArray(changeArray);
+
+
+int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+{
+    Random random = new Random();
+    int[,] array = new int[countOfRows, countOfColumns];
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 11);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();   
+    }
+}
+
+int[,] ChangeOrderElementsRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, j])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
