4cb77df274dd17cea15cf7fee65e707bc6cc1358
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:25:39 +0500

    Task_27 is done

---
+++ Homework_04/Task_27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт
+// сумму цифр в числе.
+// Например: 452 -> 11
+//           82 -> 10
+//           9012 -> 12
+
+Console.Write("Еnter a number ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int num); 
+
+if(!isParsed)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+if(num < 0)
+{
+    Console.WriteLine("Enter a positive number");
+    return;
+}
+
+int GetSumOfDigitsOfNumber(int num)
+{
+    int res = num % 10;
+
+        for (int count = 0; num / 10 > 0; count++)
+        {
+            num = num / 10;
+            res = res + num % 10;
+        }
+        return res;          
+  }
+
+int sum = GetSumOfDigitsOfNumber(num);
+
+Console.WriteLine($"The sum of the digits {num} is equal to {sum}.");
