f4611f111e71f18e3b67e2e08509efb489a60cbc
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:24:18 +0500

    Task_25 is done

---
+++ Homework_04/Task_25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B)
+// и метод, который возводит число A в натуральную степень B.
+// Задание должно быть выполнено в методе. В методе не должно 
+//быть вывода в консоль. Не использовать Math.Pow()
+
+Console.Write("Еnter a number - the base of the degree ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int num); 
+
+if(!isParsed)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+if(num==0)
+{
+    Console.WriteLine("Raising zero to a power does not make sense");
+    return;
+}
+
+Console.Write("Еnter a number - degree indicator ");
+bool isPower = int.TryParse(Console.ReadLine(), out int power);
+
+if(!isPower)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+
+double res = GetDegreeOfNumber(num, power);
+
+Console.WriteLine($"The entered number {num} to the power of {power} is {res}");
+
+double GetDegreeOfNumber(int num, int power)
+{      
+    double res = 1;
+    for (int count = 1; count <= power; count++)
+    {
+     res = res * num;
+    }               
+    return res;
+}   
+                             
+    
+    
+
