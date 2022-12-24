fb0fd00dd7419cd923b7d83a4d710f46d27cbfd1
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:17:41 +0500

    Task_13 is done

---
+++ Homework_02/Task_13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. Не использовать string[]
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Enter a number ");
+int number = int.Parse(Console.ReadLine());
+
+    while (number > 999)
+    number = number / 10;
+    { 
+       if(number < 100)
+       {
+        Console.Write("there is no third digit");
+        return;
+       }
+       else
+       {
+        int result = number % 10;
+        Console.WriteLine($"{result}");
+       }
+    }
+
