57344e1c4424df00ce78abba3b1bb53ba7ee2a38
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:16:14 +0500

    Task_10 is done

---
+++ Homework_02/Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Enter a three-digit number ");
+
+int number = int.Parse(Console.ReadLine());
+
+if(number >= 100 && number < 1000)
+{
+    Console.WriteLine($"The second digit of the number {((number % 100)/10)}");
+}
+else
+{
+    Console.WriteLine($"the entered number {number} is not three-digit");
+}
