11204ac8c4f75d1d1baa77c0d60784fe40156f37
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:59:07 +0500

    Task_64 is done

---
+++ Homework_09/Task_66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+
+Console.Write("Enter the first number M:  ");
+
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number N: ");
+
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The sum of natural numbers in the range from M to N is equal to {SumRec(m, n)}");
+
+
+
+int SumRec(int m, int n)
+{
+    if (m <= n)
+        return m + SumRec(m + 1, n);
+    else
+        return 0;
+}
