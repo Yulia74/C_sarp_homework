9e2edb2816b5e3dcc7c7604a4b9b1a6146ff78d4
Yulia lebedeva <114854080+Yulia74@users.noreply.github.com>
2022-12-25 00:22:29 +0500

    Task_23 is done

---
+++ Homework_03/Task_23/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N. Задание должно быть выполнено
+// в методе и метод должен вернуть массив чисел.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int N = InputInt();
+
+int[] cubesTable = GetTableOfCubes(N);
+
+PrintArray(cubesTable);
+
+
+
+
+
+
+int InputInt()
+{
+    Console.WriteLine("Введите целое число");
+    bool isN = int.TryParse(Console.ReadLine(), out int N);
+    if (isN)
+    {
+        return N;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+
+
+int [] GetTableOfCubes(int N)
+{
+    int[] arr = new int[N];
+    int i = 0;
+    while (i < N)
+    {
+        arr[i] = (int)(Math.Pow(i+1, 3));
+        i++;                         
+    }
+    return arr;
+}
+
+
+void PrintArray (int[] arr)
+{
+       int i = 0;
+    while (i < N)
+    {
+        Console.Write($"{arr[i]} ");
+        i++;
+    }
+}
